ff4304234a8a644d030f092dbfddd796837a1a94
Amira Ahmed <amiraalshihy@gmail.com>
2022-09-23 19:49:11 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Popcorn_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
